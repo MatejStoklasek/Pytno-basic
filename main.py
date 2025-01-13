cfc84805ba43dfca2042b85166830034897691be
matej.stoklasek <matej.stoklasek@orbiszlin.cz>
2025-01-13 15:31:00 +0100

    progress

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def tavern():
         else:
             print("Koupil jsis báječné pivo")
             money -= 20
-            add_hp(5)
+            add_hp(random.randint(-3, 5))
     elif int(choice) == 2:
         if money < 35:
             print("Nemáš dostatek peněž")
@@ -73,6 +73,7 @@ def crossroad():
     print("1 - Tréninkové hriště")
     print("2 - Krčma")
     print("3 - Souboj")
+    print("4-Konec hry")
 
     choice = input("Vyber z menu: ")
     if int(choice) == 1:
@@ -80,12 +81,31 @@ def crossroad():
     elif int(choice) == 2:
         tavern()
     elif int(choice) == 3:
+        real_fight()
         print("Budeš bojovat")
+    elif int(choice) == 4:
+        print("Ve vaší hře jste dosáhl:")
+        stats()
+        exit()
     else:
         crossroad()
 
 
+
+def fight(win_under):
+    random_chance = random.randint(1, 100)
+
+    # return random_chance < win_under
+
+    if random_chance < win_under:
+        return True
+    else:
+        return False
+
+
+
 def trainig_course():
+    global xp
     print("-----------------------")
     print("Jsi na tréninkovém hřisti")
     print("-----------------------")
@@ -94,15 +114,15 @@ def trainig_course():
     print("2 - Obrana")
 
     # Úplně nahoře je vložená knihovna
-    # import random
-    random_chance = random.randint(1, 100)
 
-    if random_chance < 50:
+
+    if fight(50):
+        xp += 1
         print("vyhral jsi")
     else:
         print("prohral jsi")
 
-
+    crossroad()
 
 def main():
     welcome_screen()
@@ -112,6 +132,31 @@ def main():
         crossroad()
     else:
         print("Hra ukončena")
+
+def real_fight():
+    global xp, hp, money
+    print("-----------------------")
+    print("Potkal jsi monstrum")
+    print("-----------------------")
+    print("\nVyber si trénink:")
+    print("1 - Útok")
+
+    print("2 - Obrana")
+
+
+
+
+
+    if fight(random.randint(40, 60)):
+        xp += random.randint (5, 30)
+        money += random.randint (5, 40)
+        print("vyhral jsi")
+    else:
+        hp -= random.randint (5, 20)
+        print("prohral jsi")
+
+    crossroad()
+
 
 
 main()
